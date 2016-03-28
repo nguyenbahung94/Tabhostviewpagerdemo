1da5937875a057a42c42f21eafa6c679dce52a1a
nguyenbahung94 <mr.hungcity@gmail.com>
2016-03-28 11:43:28 +0700

    Delete FragmentHome.java

--- app/src/main/java/com/example/android/tabhostviewpartslidingmenu/FragmentHome.java
+++
@@ -1,9 +0,0 @@
-package com.example.android.tabhostviewpartslidingmenu;
-
-import android.app.Fragment;
-
-/**
- * Created by everything on 3/28/2016.
- */
-public class FragmentHome extends Fragment {
-}
